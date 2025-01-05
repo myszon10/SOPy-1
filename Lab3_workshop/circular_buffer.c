ec2caa3d806ff8486aacdb24ad5a7ea6aea99cfa
szymon <szymon.mazulis@gmail.com>
2025-01-05 18:50:37 +0100

    Lab3_workshop_etap1

---
+++ Lab3_workshop/circular_buffer.c
@@ -0,0 +1,77 @@
+#include "circular_buffer.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+
+circular_buffer* circular_buffer_init() 
+{
+    circular_buffer *buffer = malloc(sizeof(circular_buffer));
+    if(buffer == NULL)
+        ERR("malloc");
+
+    buffer->head = 0;
+    buffer->tail = 0;
+    buffer->count = 0;
+    
+    if(pthread_mutex_init(&buffer->mxBuffer, NULL) != 0)
+    {
+        free(buffer);
+        ERR("pthread_mutex_init");
+    }
+    return buffer;
+}
+
+void circular_buffer_deinit(circular_buffer *bufferArgs) 
+{
+    if(bufferArgs == NULL)
+        return;
+
+    pthread_mutex_destroy(&bufferArgs->mxBuffer);
+    free(bufferArgs);
+}
+
+void circular_buffer_enqueue(circular_buffer *bufferArgs, char *item) 
+{
+    if(bufferArgs == NULL)
+        ERR("Nullptr passed as argument");
+
+    while(bufferArgs->count == BUFFER_SIZE) // bufor jest pełny
+    {
+        // ERR("Cannot add to a full buffer");
+        usleep(5000); // wait 5ms (busy waiting)
+    }
+
+    pthread_mutex_lock(&bufferArgs->mxBuffer);
+    bufferArgs->buffer[bufferArgs->head] = item; // dodanie elementu na pozycji head 
+
+    bufferArgs->head = (bufferArgs->head + 1) % BUFFER_SIZE; // cykliczne przesunięcie head
+
+    bufferArgs->count++; // zwiększenie licznika elementów
+    pthread_mutex_unlock(&bufferArgs->mxBuffer);
+}
+
+char* circular_buffer_dequeue(circular_buffer *bufferArgs) 
+{
+    if(bufferArgs == NULL)
+        ERR("Nullptr passed as argument");
+
+    while(bufferArgs->count == 0) // bufor jest pusty
+    {
+        // ERR("Cannot remove from an empty buffer");
+        usleep(5000); // wait 5ms (busy waiting)
+    }
+
+    pthread_mutex_lock(&bufferArgs->mxBuffer);
+    char *item = bufferArgs->buffer[bufferArgs->tail];
+
+    bufferArgs->tail = (bufferArgs->tail + 1) % BUFFER_SIZE; // przesunięcie wskaźnika tail
+
+    bufferArgs->count--; // zmniejszenie liczby elementów
+    pthread_mutex_unlock(&bufferArgs->mxBuffer);
+
+    return item;
+}
---
+++ Lab3_workshop_dane/circular_buffer.c
@@ -0,0 +1,21 @@
+#include "circular_buffer.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+
+circular_buffer* circular_buffer_init() {
+    // TODO
+}
+
+void circular_buffer_deinit(circular_buffer *buffer) {
+    // TODO
+}
+
+void circular_buffer_enqueue(circular_buffer *buffer, const char *item) {
+    // TODO
+}
+
+char* circular_buffer_dequeue(circular_buffer *buffer) {
+    // TODO
+}
