2b9290cd8fbdc7e910c5fb60189d3d2d5a85d9d1
szymon <szymon.mazulis@gmail.com>
2025-01-05 23:06:17 +0100

    Lab3_workshop_etap3

--- Lab3_workshop/etap3.c
+++ Lab3_workshop/etap3.c
@@ -3,11 +3,15 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h> // do funkcji usleep, read i close
 #include <string.h>
 #include <stdbool.h>
 #include <limits.h> // PATH_MAX
+#include <fcntl.h> // do funkcji open
+#include <ctype.h> // do funkcji isalpha, toupper
 #include "circular_buffer.h"
+
+#define MUTEX_COUNT 52
 
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 
@@ -20,16 +24,30 @@ typedef struct worker_args {
     int *processedFiles; // liczba przetworzonych plików txt
     int *totalFiles;     // całkowita liczba plików
     pthread_mutex_t *mxProcessed;
+    int *alphabetCounter;
+    pthread_mutex_t *mutexes; // tablica mutexów dla każdego znaku
+    pthread_mutex_t *mxPrint; // mutex do synchronizacji wypisywania
 } worker_args_t;
 
 void ReadArgs(int argc, char **argv, char *startPath, int *threadCount);
 void explore_directory(const char* dir_path, circular_buffer *buffer, int *totalFiles);
 void* worker_func(void* voidArgs);
+void process_file(const char* file_path, int *alphabetCounter, pthread_mutex_t *mutexes, int worker_id, pthread_mutex_t *mxPrint);
 
 int main(int argc, char **argv) 
 {
     int threadCount;
     char startPath[20];
+    int alphabetCounter[52] = {0};
+    pthread_mutex_t mutexes[MUTEX_COUNT];
+    pthread_mutex_t mxPrint = PTHREAD_MUTEX_INITIALIZER;
+
+    for(int i = 0; i < MUTEX_COUNT; i++)
+    {
+        if(pthread_mutex_init(&mutexes[i], NULL) != 0)
+            ERR("Setup of letter mutexes");
+    }
+    
     ReadArgs(argc, argv, startPath, &threadCount);
 
     bool quitFlag = false;
@@ -55,6 +73,9 @@ int main(int argc, char **argv)
         threadArgs[i].totalFiles = &totalFiles;
         threadArgs[i].mxQuitFlag = &mxQuitFlag;
         threadArgs[i].mxProcessed = &mxProcessed;
+        threadArgs[i].alphabetCounter = alphabetCounter;
+        threadArgs[i].mutexes = mutexes;
+        threadArgs[i].mxPrint = &mxPrint;
     }
 
     for(int i = 0; i < threadCount; i++)
@@ -88,6 +109,13 @@ int main(int argc, char **argv)
     }
 
     // Zwolnienie zasobów
+    for(int i = 0; i < MUTEX_COUNT; i++)
+    {
+        if(pthread_mutex_destroy(&mutexes[i]) != 0)
+            ERR("Mutex destroy");
+    }
+
+    pthread_mutex_destroy(&mxPrint);
     circular_buffer_deinit(buffer);
     pthread_mutex_destroy(&mxQuitFlag);
     pthread_mutex_destroy(&mxProcessed);
@@ -158,7 +186,8 @@ void* worker_func(void* voidArgs)
         file_path = circular_buffer_dequeue(args->buffer);
         if(file_path != NULL)
         {
-            printf("Pracownik %d reprezentuje plik %s\n", args->worker_id, file_path);
+            //printf("Pracownik %d reprezentuje plik %s\n", args->worker_id, file_path);
+            process_file(file_path, args->alphabetCounter, args->mutexes, args->worker_id, args->mxPrint);
             free(file_path); // zwolnienie pamięci
 
             pthread_mutex_lock(args->mxProcessed);
@@ -173,4 +202,65 @@ void* worker_func(void* voidArgs)
     }
 
     return NULL;
+}
+
+void process_file(const char* file_path, int *alphabetCounter, pthread_mutex_t *mutexes, int worker_id, pthread_mutex_t *mxPrint)
+{
+    int fd = open(file_path, O_RDONLY);
+    if(fd == -1)
+        ERR("Cannot open file");
+
+    int localCounter[52] = {0}; // lokalny licznik liter
+    char buffer[1024]; // bufor do odczytu danych
+    ssize_t bytesRead;
+
+    while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0)
+    {
+        for(ssize_t i = 0; i < bytesRead; i++)
+        {
+            char c = buffer[i];
+            if(isalpha(c))
+            {
+                int index;
+                if(isupper(c))
+                    index = c - 'A'; // A-Z: indeksy 0-25
+                else
+                    index = c - 'a' + 26; // a-z: indeksy 26-51
+
+                localCounter[index]++;
+            }
+        }
+    }
+    if(bytesRead == -1)
+        ERR("read");
+
+    close(fd);
+
+    // Akturalizacja globalnego licznika z użyciem mutexów
+    for(int i = 0; i < 52; i++)
+    {
+        if(localCounter[i] > 0)
+        {
+            pthread_mutex_lock(&mutexes[i]);
+            alphabetCounter[i] += localCounter[i];
+            pthread_mutex_unlock(&mutexes[i]);
+        }
+    }
+
+    pthread_mutex_lock(mxPrint);
+    printf("Pracownik nr %d zakończył zliczanie liter w pliku %s\n", worker_id, file_path);
+    printf("Wyniki:\n");
+    for(int i = 0; i < 26; i++)
+    {
+        if(localCounter[i] != 0)
+            printf("%c=%d ", 'A' + i, localCounter[i]);
+    }
+    printf("\n");
+    for(int i = 26; i < 52; i++)
+    {  
+        if(localCounter[i] != 0)
+            printf("%c=%d ", 'a' + (i - 26), localCounter[i]);
+    }
+    printf("\n\n");
+    pthread_mutex_unlock(mxPrint);
 }
